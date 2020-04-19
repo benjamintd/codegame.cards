24ed13e0fbd70b868a2c35d6ef1279eeaa8173ad
Benjamin TD <benjamin.tdm@gmail.com>
2020-04-19 16:40:48 +0200

    container's too small

--- components/LobbyGameRow.tsx
+++ components/LobbyGameRow.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 export default ({ game }: { game: IGame }) => {
   return (
     <Link href="/[gameId]" as={`/${game.id}`}>
-      <div className="border-b-4 border-gray-300 border border-gray-600 rounded p-2 cursor-pointer w-64 hover:bg-white">
+      <div className="border-b-4 border-gray-300 border border-gray-600 rounded p-2 cursor-pointer w-84 hover:bg-white">
         <div className="text-sm mb-2 tracking-wide font-bold">{game.id}</div>
         <div className="flex">
-          <div className="mr-2">{(game.players || []).length} players</div>
+          <div className="mr-2">
+            {Object.keys(game.players || {}).length} players
+          </div>
           <div className="mr-2">{game.options.mode}</div>
           <div>{game.options.language === "en" ? "🇬🇧" : "🇫🇷"}</div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       colors: {
         accent: "#2552f5",
       },
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+      },
     },
   },
   variants: {},
