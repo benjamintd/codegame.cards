4b12390aa14d29ade62d4078c45683c35938a5a3
Benjamin TD <benjamin.tdm@gmail.com>
2020-04-22 16:25:43 +0200

    fix #3 and some cosmetic changes

--- components/LobbyGameRow.tsx
+++ components/LobbyGameRow.tsx
@@ -5,14 +5,27 @@ import Link from "next/link";
 export default ({ game }: { game: IGame }) => {
   return (
     <Link href="/[gameId]" as={`/${game.id}`}>
-      <div className="border-b-4 border-gray-300 border border-gray-600 rounded p-2 cursor-pointer w-84 bg-white hover:shadow-md">
-        <div className="text-sm mb-2 tracking-wide font-mono">{game.id}</div>
-        <div className="flex">
-          <div className="mr-2">
-            {Object.keys(game.players || {}).length} players
+      <div className="border-b-4 border-gray-300 border border-gray-600 rounded p-2 cursor-pointer w-84 hover:bg-white hover:shadow-md">
+        <div className="flex mb-2 text-sm">
+          <span className="tracking-wide font-mono">{game.id}</span>
+
+          {(game.turns || []).length > 0 && (
+            <span className="ml-auto text-green-500">started</span>
+          )}
+        </div>
+        {game.players && (
+          <div className="mb-2">
+            👤
+            {Object.values(game.players)
+              .map((p) => p.name)
+              .join(", ")}
           </div>
-          <div className="mr-2">{game.options.mode}</div>
-          <div>{getFlag(game.options.language)}</div>
+        )}
+        <div className="inline-block rounded-full border border-gray-600 text-gray-600 px-2">
+          {game.options.mode}{" "}
+          <span className="inline-block text-lg align-middle">
+            {getFlag(game.options.language)}
+          </span>
         </div>
       </div>
     </Link>
