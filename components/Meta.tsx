b60dd76eb94dfd5233b58622de8127470f025ad8
Benjamin TD <benjamin.tdm@gmail.com>
2020-05-26 15:00:02 +0200

    shameless description copypaste :)

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -7,18 +7,24 @@ export default () => {
       <title>Codenames - Play Online</title>
       <meta
         name="description"
-        content="Play Codenames online. Guess with friends in this real-time adaptation of the popular tabletop game."
+        content="Play Codenames online, a multiplayer word game available on phone, tablet & desktop."
       />
       <link rel="shortcut icon" href="/icon-192.png" />
       <meta property="og:title" content="codenames.cards" />
-      <meta property="og:description" content="Play Codenames online." />
+      <meta
+        property="og:description"
+        content="Play Codenames online, a multiplayer word game available on phone, tablet & desktop."
+      />
       <meta
         property="og:image"
         content="https://codenames.cards/og-image.png"
       />
       <meta property="og:url" content="https://codenames.cards" />
       <meta name="twitter:title" content="Codenames.cards" />
-      <meta name="twitter:description" content="Play Codenames online." />
+      <meta
+        name="twitter:description"
+        content="Play Codenames online, a multiplayer word game available on phone, tablet & desktop."
+      />
       <meta
         name="twitter:image"
         content="https://codenames.cards/og-image.png"
