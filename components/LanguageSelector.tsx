32896034530b2c6590ba675f972aeaffb52d69ab
Benjamin TD <benjamin.tdm@gmail.com>
2020-10-28 16:01:53 +0100

    language selector as menu to allow prefetching

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,6 +1,7 @@
+import { Menu, Transition } from "@headlessui/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-
 
 const Languages = {
   en: "English",
@@ -14,22 +15,67 @@ export default function LanguageSelector() {
   const router = useRouter();
 
   return (
-    <label htmlFor="lang">
-      <span className="sr-only">Choose a language</span>
-      <select
-        className="form-select bg-transparent"
-        value={router.locale}
-        onChange={e => {
-          router.push(router.pathname, router.asPath, { locale: e.target.value });
-        }}
-      >
-        {Object.keys(Languages).map(value => (
-          <option key={value} value={value}>
-            {Languages[value]}
-          </option>
-        ))}
-      </select>
-    </label>
+    <div className="flex items-center justify-center">
+      <div className="relative inline-block text-left">
+        <Menu>
+          {({ open }) => (
+            <>
+              <span className="rounded-md shadow-sm">
+                <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
+                  <span>{Languages[router.locale]}</span>
+                  <svg
+                    className="w-5 h-5 ml-2 -mr-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      fillRule="evenodd"
+                    />
+                  </svg>
+                </Menu.Button>
+              </span>
+
+              <Transition
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+                show={open}
+              >
+                <Menu.Items
+                  className="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                >
+                    {Object.keys(Languages).map((value) => (
+                      <Menu.Item key={value}>
+                        {({ active }) => (
+                          <Link
+                            as={router.asPath}
+                            href={router.pathname}
+                            locale={value}
+                            passHref
+                          >
+                            <a className={`${
+                                active
+                                  ? "bg-gray-200 text-gray-900"
+                                  : "text-gray-700"
+                              } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
+                            >
+                              {Languages[value]}
+                            </a>
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    ))}
+                </Menu.Items>
+              </Transition>
+            </>
+          )}
+        </Menu>
+      </div>
+    </div>
   );
 }
-
